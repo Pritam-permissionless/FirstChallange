a7c01ce32c41dce17cf462f1ac63633f90dfadf7
Pritam Das <115615106+Pritam-permissionless@users.noreply.github.com>
2022-10-12 18:06:18 +0530

    Delete Task 2/src directory

--- Task 2/src/routers/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const userDetails = require('../controller/userController')
-
-router.post('/userRegister', userDetails.userDetails)
-
-
-module.exports = router
