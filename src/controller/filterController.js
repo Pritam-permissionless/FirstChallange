30f0e868486a025f7de218b5b77de740aa25d0b3
Pritam Das <115615106+Pritam-permissionless@users.noreply.github.com>
2022-10-12 14:42:28 +0530

    Update filterController.js

--- src/controller/filterController.js
+++ src/controller/filterController.js
@@ -2,9 +2,9 @@ const filterModel = require("../models/filterSchema");
 
 const filter = async function (req, res) {
   try {
-    // const data = await filterModel.find(req.body);
+    // const data = await filterModel.find(req.query);
 
-    res.status(200).send({ status: true, data: req.body });
+    res.status(200).send({ status: true, data: req.query});
   } catch (error) {
     res.status(400).send({ status: false, message: error.message });
   }
